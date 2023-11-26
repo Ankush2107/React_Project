e7599514abc8d78512b31cdf8ae7d64abfa7060a
Ankush2107 <ankush.kumar210701@gmail.com>
2023-11-26 13:52:07 +0530

    Now working with unsplash api

--- Image_Search_App/src/App.jsx
+++ Image_Search_App/src/App.jsx
@@ -1,11 +1,40 @@
+import React, { useRef }  from 'react'
 import './App.css'
-
+import { Form } from 'react-bootstrap'
 function App() {
+    const searchInput = useRef(null);
+
+    const API_URL = "https://api.unsplash.com/search/photos";
+
+    const handleSearch = () => {
+      event.preventDefault();
+      console.log(searchInput);
+    }
+
+    const handleSection = (seleection) => {
+      searchInput.current.value = seleection
+    }
 
   return (
-    <>
-      
-    </>
+    <div className='container'>
+        <h1 className='title'>Image Search</h1>
+        <div className='search-section'>
+            <Form onSubmit={handleSearch}>
+              <Form.Control 
+                type='search'
+                placeholder='Type something for search...'
+                className='search-input'
+                ref={searchInput}
+              />
+            </Form>
+        </div>
+        <div className='filters'>
+                <div onClick={() => handleSection('nature')}>Nature</div>
+                <div onClick={() => handleSection('birds')}>Birds</div>
+                <div onClick={() => handleSection('cats')}>Cats</div>
+                <div onClick={() => handleSection('shoes')}>Shoes</div>
+        </div>
+    </div>
   )
 }
 
